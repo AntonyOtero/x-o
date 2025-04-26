6cbd1e2afef76755fa056ad69c0dab0051847d31
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-04-26 18:21:21 -0400

    refactor: Rewrite code from scratch

--- scripts/main.js
+++ scripts/main.js
@@ -1,213 +1,213 @@
-const createPlayer = (name, token, piece) => {
+const createPlayer = (name, token) => {
     let score = 0;
     const getScore = () => score;
     const addScore = () => ++score;
     return {
         name,
         token,
-        piece,
         getScore,
         addScore,
     };
 }
 
 const gameLogic = (() => {
-    const playerOne = createPlayer("Antony", "x");
-    const playerTwo = createPlayer("Tony", "o");
-    let currentPlayer = playerOne;
-    let winCondition = undefined;
+    const newGame = () => {
+        if (document.querySelector(".board")) {
+            document.querySelector(".container").classList.toggle("disable");
+            gameUI.removeBoardUI();
+            gameUI.removeNotification();
+        }
+        gameUI.displayPlayerInfoUI();
+    }
     const startGame = () => {
-        currentPlayer = playerOne;
-        displayController.displayBoard();
-    };
+        // Initialize gameMemory
+        gameMemory.initializeMemory();
+        const PlayerOneInput = document.querySelector("#player-one");
+        const PlayerTwoInput = document.querySelector("#player-two");
+        gameMemory.updatePlayerOne(createPlayer(PlayerOneInput.value, "X"));
+        gameMemory.updatePlayerTwo(createPlayer(PlayerTwoInput.value, "O"));
+        // set current player
+        gameMemory.initializeCurrentPlayer();
+        // remove player info UI
+        gameUI.removePlayerInfoUI();
+        // Display gameboard UI
+        gameUI.displayBoardUI();
+        // Wait for player input
+    }
     const restartGame = () => {
-        currentPlayer = playerOne;
-        gameboard.restartMemory();
-        displayController.updateBoard();
-        displayController.removeNotification();
+        gameUI.removeNotification();
+        gameMemory.initializeMemory();
+        gameMemory.initializeCurrentPlayer();
+        gameUI.restartBoardUI();
     }
-    const endGame = (type) => {
-        if (type) {
-            displayController.displayNotification(`${currentPlayer.name} wins!`);
-            displayController.displayWinCondition(getWinCondition());
+    const placeTokenAtIndex = (i) => {
+        gameMemory.updateMemory(i);
+        gameUI.updateBoardUI();
+        if (gameMemory.isWin()) {
+            gameUI.highlightWin(gameMemory.getWinningCondition());
+            gameUI.displayNotification(1);
+        } else if (gameMemory.isTie()) {
+            gameUI.displayNotification(0);
         } else {
-            displayController.displayNotification("It's a Tie! No moves remaining.");
+            gameMemory.updateCurrentPlayer();
         }
-    };
-    const getCurrentPlayer = () => currentPlayer;
-    const updateCurrentPlayer = () => {
-        currentPlayer = (currentPlayer == playerOne) ? playerTwo : playerOne
-    };
-
-    const placeTokenAt = (unit) => {
-        gameboard.updateMemory(unit, currentPlayer.token);
-
-        if (checkWin()) {
-            endGame(1);
-        } else if (checkTie()) {
-            endGame(0)
-        } else {
-            updateCurrentPlayer();
-        }
-    };
-
-    const checkWin = () => {
-        const winningConditions = [
-            [0, 1, 2], //Top-Across
-            [3, 4, 5], //Middle-Across
-            [6, 7, 8], //Bottom-Across
-            [0, 3, 6], //Left-Down
-            [1, 4, 7], //Middle-Down
-            [2, 5, 8], //Right-Down
-            [0, 4, 8], //LR-Diagonal
-            [2, 4, 6], //RL-Diagonal
-        ];
-
-        const checkedConditions = winningConditions.map((condition) => {
-            return condition.map((position) => {
-                return gameboard.getMemory()[position] === gameLogic.getCurrentPlayer().token;
-            }).reduce((prevBool, currBool) => prevBool && currBool);
-        });
-
-        if (checkedConditions.indexOf(true) != -1) {
-            winCondition = winningConditions[checkedConditions.indexOf(true)];
-        }
-
-        return checkedConditions.includes(true);
-    };
-
-    const getWinCondition = () => winCondition;
-
-    const checkTie = () => {
-        return !gameboard.getMemory().includes("") && !checkWin();
-    };
+    }
 
     return {
+        newGame,
         startGame,
-        endGame,
         restartGame,
-        getCurrentPlayer,
-        updateCurrentPlayer,
-        placeTokenAt,
-        checkWin,
-        getWinCondition,
-        checkTie,
-    };
-})();
-
-const gameboard = (() => {
-    let memory = new Array(9).fill("");
-    const getMemory = () => memory;
-    const updateMemory = (unit, token) => {
-        memory[unit] = token;
-        displayController.updateBoard();
-    };
-    const restartMemory = () => memory = new Array(9).fill("");
-    return {
-        getMemory,
-        updateMemory,
-        restartMemory,
+        placeTokenAtIndex,
     }
 })();
 
-const displayController = (() => {
-    const Container = document.querySelector(".container");
-    const displayBoard = () => {
-        const Gameboard = document.createElement("div")
-        Gameboard.classList.add("gameboard");
-        Container.appendChild(Gameboard);
-        gameboard.getMemory().forEach((unit) => {
-            const GameboardUnit = document.createElement("div");
-            GameboardUnit.classList.add("unit");
-            GameboardUnit.innerHTML = unit;
-            Gameboard.appendChild(GameboardUnit);
-        });
-        document.querySelectorAll(".unit").forEach((unit, i) => {
-            unit.addEventListener("click", e => {
-                gameLogic.placeTokenAt(i);
+const gameMemory = (() => {
+    let playerOne, playerTwo, currentPlayer, winningCondition, memory;
+    const winConditions = [
+        [0, 1, 2], //Top-Across
+        [3, 4, 5], //Middle-Across
+        [6, 7, 8], //Bottom-Across
+        [0, 3, 6], //Left-Down
+        [1, 4, 7], //Middle-Down
+        [2, 5, 8], //Right-Down
+        [0, 4, 8], //LR-Diagonal
+        [2, 4, 6], //RL-Diagonal
+    ];
+    const initializeMemory = () => memory = new Array(9).fill("");
+    const initializeCurrentPlayer = () => currentPlayer = getPlayerOne();
+    const getPlayerOne = () => playerOne;
+    const getPlayerTwo = () => playerTwo;
+    const getCurrentPlayer = () => currentPlayer;
+    const getWinningCondition = () => winningCondition;
+    const getMemory = () => memory;
+    const updatePlayerOne = (newPlayer) => playerOne = newPlayer;
+    const updatePlayerTwo = (newPlayer) => playerTwo = newPlayer;
+    const updateCurrentPlayer = () => currentPlayer = (currentPlayer == playerOne) ? playerTwo : playerOne;
+    const updateWinningCondition = (condition) => winningCondition = condition;
+    const updateMemory = (i) => {
+        if (memory[i] == "") memory[i] = getCurrentPlayer().token;
+    }
+    const isWin = () => {
+        const winCheck = winConditions.map((condition) => {
+            return condition.map(winIndex => {
+                return memory[winIndex] === currentPlayer.token;
+            }).reduce((prevWinBool, curWinBool) => {
+                return prevWinBool && curWinBool;
             });
         });
+
+        if (winCheck.includes(true)) {
+            updateWinningCondition(winConditions[winCheck.indexOf(true)]);
+        }
+
+        return winCheck.includes(true);
     }
-    const removeBoard = () => {
-        document.querySelector(".gameboard").remove();
+    const isTie = () => {
+        return !memory.includes("");
     }
-    const updateBoard = () => {
-        removeBoard();
-        displayBoard();
+
+    return {
+        initializeMemory,
+        initializeCurrentPlayer,
+        getPlayerOne,
+        getPlayerTwo,
+        getCurrentPlayer,
+        getWinningCondition,
+        getMemory,
+        updatePlayerOne,
+        updatePlayerTwo,
+        updateCurrentPlayer,
+        updateWinningCondition,
+        updateMemory,
+        isWin,
+        isTie,
     }
-    const displayPlayerInfo = () => {
+})();
+
+const gameUI = (() => {
+    const Container = document.querySelector(".container");
+    const displayPlayerInfoUI = () => {
         const PlayerInfo = document.createElement("div");
-        PlayerInfo.classList = "player-info notification";
-        Container.appendChild(PlayerInfo);
+        PlayerInfo.setAttribute("id", "player-info");
+        PlayerInfo.classList.add("card");
         PlayerInfo.innerHTML = `
             <p class="player">
                 Player One is <input id="player-one" type="text" placeholder="P1 Name Here"> as X
             </p>
-            <p class="player player-two">
+            <p class="player">
                 Player Two is <input id="player-two" type="text" placeholder="P2 Name Here"> as O
             </p>
-            <button class="btn start-btn">Start</button>
+            <button class="btn start-btn" onclick="gameLogic.startGame()">Start</button>
         `
+        Container.appendChild(PlayerInfo);
     }
-    const displayNotification = (message) => {
-        const Notification = document.createElement("div");
-        Notification.classList.add("notification");
-        document.body.appendChild(Notification);
-        Notification.textContent = message;
-        Container.classList.toggle("disable");
-        const ReplayButton = document.createElement("button");
-        ReplayButton.classList.add("btn");
-        ReplayButton.textContent = "Rematch";
-        Notification.appendChild(ReplayButton);
-        ReplayButton.addEventListener("click", e => {
-            gameLogic.restartGame();
+    const displayBoardUI = () => {
+        const Board = document.createElement("div");
+        Board.classList.add("board");
+        Container.appendChild(Board);
+        gameMemory.getMemory().forEach((cell, i) => {
+            const Unit = document.createElement("div");
+            Unit.classList.add("unit");
+            Unit.textContent = cell;
+            Unit.setAttribute("onclick", `gameLogic.placeTokenAtIndex(${i})`);
+            Board.appendChild(Unit);
         });
+    }
+    const displayNotification = (type) => {
+        Container.classList.toggle("disable");
+        const Notification = document.createElement("div");
+        Notification.classList = "notification card";
+        document.body.appendChild(Notification);
+        if (type == 1) {
+            Notification.textContent = `${gameMemory.getCurrentPlayer().name} won!`;
+        } else {
+            Notification.textContent = "It's a tie! No more moves possible."
+        }
+        const RematchButton = document.createElement("button");
+        RematchButton.classList.add("btn");
+        RematchButton.textContent = "Rematch";
+        RematchButton.setAttribute("onclick", "gameLogic.restartGame()");
+        const NewGameButton = document.createElement("button");
+        NewGameButton.classList.add("btn");
+        NewGameButton.textContent = "New Game";
+        NewGameButton.setAttribute("onclick", "gameLogic.newGame()");
+        Notification.appendChild(RematchButton);
+        Notification.appendChild(NewGameButton);
+    }
+    const removePlayerInfoUI = () => {
+        document.querySelector("#player-info").remove();
+    }
+    const removeBoardUI = () => {
+        document.querySelector(".board").remove();
     }
     const removeNotification = () => {
         document.querySelector(".notification").remove();
-        Container.classList.toggle("disable");
     }
-    const displayWinCondition = (winCondition) => {
-        const Units = document.querySelectorAll(".unit");
-        winCondition.forEach(winUnitIndex => {
-            Units[winUnitIndex].classList.toggle("highlight");
-        });
+    const updateBoardUI = () => {
+        removeBoardUI();
+        displayBoardUI();
+    }
+    const highlightWin = (condition) => {
+        const WinningUnits = document.querySelectorAll(".unit");
+        condition.forEach(winIndex => WinningUnits[winIndex].classList.add("highlight"));
+    }
+    const restartBoardUI = () => {
+        removeBoardUI();
+        displayBoardUI();
+        document.querySelector(".container").classList.toggle("disable");
     }
 
     return {
-        displayBoard,
-        removeBoard,
-        updateBoard,
-        displayPlayerInfo,
+        displayPlayerInfoUI,
+        displayBoardUI,
         displayNotification,
+        removePlayerInfoUI,
+        removeBoardUI,
         removeNotification,
-        displayWinCondition,
-    };
+        updateBoardUI,
+        highlightWin,
+        restartBoardUI,
+    }
 })();
 
-displayController.displayPlayerInfo();
-// gameLogic.startGame();
-// displayController.displayNotification();
-// TIE GAME
-// gameLogic.placeTokenAt(0);
-// gameLogic.placeTokenAt(3);
-// gameLogic.placeTokenAt(1);
-// gameLogic.placeTokenAt(4);
-// gameLogic.placeTokenAt(5);
-// gameLogic.placeTokenAt(2);
-// gameLogic.placeTokenAt(6);
-// gameLogic.placeTokenAt(7);
-// gameLogic.placeTokenAt(8);
-// Player One Wins
-// gameLogic.placeTokenAt(0);
-// gameLogic.placeTokenAt(1);
-// gameLogic.placeTokenAt(4);
-// gameLogic.placeTokenAt(3);
-// gameLogic.placeTokenAt(8);
-// Player Two Wins
-// gameLogic.placeTokenAt(0);
-// gameLogic.placeTokenAt(2);
-// gameLogic.placeTokenAt(1);
-// gameLogic.placeTokenAt(4);
-// gameLogic.placeTokenAt(3);
-// gameLogic.placeTokenAt(6);
+gameLogic.newGame();
